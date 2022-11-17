5d9b0e12cf8d1a590a589330a6a50b7904aa9b1d
root <root@localhost.localdomain>
2022-11-17 11:20:18 +0800

    初始化

---
+++ src/.umi/core/routes.ts
@@ -0,0 +1,106 @@
+// @ts-nocheck
+import React from 'react';
+import { ApplyPluginsType, dynamic } from '/home/ubuntu/banaweb/node_modules/umi/node_modules/@umijs/runtime';
+import * as umiExports from './umiExports';
+import { plugin } from './plugin';
+import LoadingComponent from '@/components/PageLoading/index';
+
+export function getRoutes() {
+  const routes = [
+  {
+    "path": "/",
+    "component": dynamic({ loader: () => import(/* webpackChunkName: 'layouts__BlankLayout' */'/home/ubuntu/banaweb/src/layouts/BlankLayout'), loading: LoadingComponent}),
+    "routes": [
+      {
+        "path": "/user",
+        "component": dynamic({ loader: () => import(/* webpackChunkName: 'layouts__UserLayout' */'/home/ubuntu/banaweb/src/layouts/UserLayout'), loading: LoadingComponent}),
+        "routes": [
+          {
+            "name": "login",
+            "path": "/user/login",
+            "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__User__login' */'/home/ubuntu/banaweb/src/pages/User/login'), loading: LoadingComponent}),
+            "exact": true
+          }
+        ]
+      },
+      {
+        "path": "/",
+        "component": dynamic({ loader: () => import(/* webpackChunkName: 'layouts__SecurityLayout' */'/home/ubuntu/banaweb/src/layouts/SecurityLayout'), loading: LoadingComponent}),
+        "routes": [
+          {
+            "path": "/",
+            "component": dynamic({ loader: () => import(/* webpackChunkName: 'layouts__BasicLayout' */'/home/ubuntu/banaweb/src/layouts/BasicLayout'), loading: LoadingComponent}),
+            "authority": [
+              "admin",
+              "user"
+            ],
+            "routes": [
+              {
+                "path": "/",
+                "redirect": "/welcome",
+                "exact": true
+              },
+              {
+                "path": "/welcome",
+                "name": "welcome",
+                "icon": "smile",
+                "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__Welcome' */'/home/ubuntu/banaweb/src/pages/Welcome'), loading: LoadingComponent}),
+                "exact": true
+              },
+              {
+                "path": "/admin",
+                "name": "admin",
+                "icon": "crown",
+                "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__Admin' */'/home/ubuntu/banaweb/src/pages/Admin'), loading: LoadingComponent}),
+                "authority": [
+                  "admin"
+                ],
+                "routes": [
+                  {
+                    "path": "/admin/sub-page",
+                    "name": "sub-page",
+                    "icon": "smile",
+                    "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__Welcome' */'/home/ubuntu/banaweb/src/pages/Welcome'), loading: LoadingComponent}),
+                    "authority": [
+                      "admin"
+                    ],
+                    "exact": true
+                  }
+                ]
+              },
+              {
+                "name": "list.table-list",
+                "icon": "table",
+                "path": "/list",
+                "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__TableList' */'/home/ubuntu/banaweb/src/pages/TableList'), loading: LoadingComponent}),
+                "exact": true
+              },
+              {
+                "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__404' */'/home/ubuntu/banaweb/src/pages/404'), loading: LoadingComponent}),
+                "exact": true
+              }
+            ]
+          },
+          {
+            "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__404' */'/home/ubuntu/banaweb/src/pages/404'), loading: LoadingComponent}),
+            "exact": true
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__404' */'/home/ubuntu/banaweb/src/pages/404'), loading: LoadingComponent}),
+    "exact": true
+  }
+];
+
+  // allow user to extend routes
+  plugin.applyPlugins({
+    key: 'patchRoutes',
+    type: ApplyPluginsType.event,
+    args: { routes },
+  });
+
+  return routes;
+}
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/home/ubuntu/banaweb/node_modules/react-helmet';
